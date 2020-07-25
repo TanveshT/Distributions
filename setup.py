f3173f095a0f454a73eddbb72414253e3a5bc06c
Tanvesh Takawale <tanveshtakawale26@gmail.com>
2020-07-26 01:52:16 +0530

    Fix: Added more details to the setup.py file

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
 
-setup(name='distributions',
+setup(name='distributions-statistics',
       version='1.0',
       description='Gaussian distributions, Binomial Distributions',
       packages=['distributions'],
+      author = 'Tanvesh Takawale',
+      author_email = 'tanveshtakawale26@gmail.com',
       zip_safe=False)
