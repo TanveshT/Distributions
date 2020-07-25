ce9363c9245705309ac37fd06b6a3a158fd55526
Tanvesh Takawale <tanveshtakawale26@gmail.com>
2020-07-26 01:54:38 +0530

    Fix: Setup.py changed the packages attribute

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='distributions-statistics',
       version='1.0',
       description='Gaussian distributions, Binomial Distributions',
-      packages=['distributions'],
+      packages=['distributions-statistics'],
       author = 'Tanvesh Takawale',
       author_email = 'tanveshtakawale26@gmail.com',
       zip_safe=False)
