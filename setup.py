bcb56ceb5576bdcf99a946ccc2f859560c55d439
Tanvesh Takawale <tanveshtakawale26@gmail.com>
2020-07-26 01:48:24 +0530

    Fix: Added description to the setup.py file

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(name='distributions',
       version='1.0',
-      description='Gaussian distributions',
+      description='Gaussian distributions, Binomial Distributions',
       packages=['distributions'],
       zip_safe=False)
